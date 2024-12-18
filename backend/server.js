11a72e26239f31da155ed9ff8ddff0fca0873b43
rishavtiwari22 <tiwaririshav463@gmail.com>
2024-12-18 17:25:20 +0530

    made changes in the mongoDB conection

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
